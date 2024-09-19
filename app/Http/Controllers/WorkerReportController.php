8b4fb4fb5d6a4ca8ad3cc55c6cab768f876c6b00
Vinicius Padovan <vini.vptds@gmail.com>
2024-09-19 18:27:32 -0300

    Update report controller to use queue size as condition to a new request

--- app/Http/Controllers/WorkerReportController.php
+++ app/Http/Controllers/WorkerReportController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Jobs\WorkersWithMissingVaccinesReportJob;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Queue;
 use Illuminate\Support\Facades\Storage;
 
 class WorkerReportController extends Controller
@@ -20,15 +22,33 @@ class WorkerReportController extends Controller
             ];
         }, $files);
 
-        return view('workers.report')->with('files', $files);
+        $queue = App::make('queue.connection');
+        $size = $queue->size();
+
+        $latestFile = null;
+
+        if ($size > 0) {
+            $latestFile = false;
+        } elseif (!empty($files)) {
+            $latestFile = $files[0];
+        }
+
+        return view('workers.report')->with('files', $files)->with('latestFile', $latestFile);
     }
 
     public function makeReport()
     {
-        $job = new WorkersWithMissingVaccinesReportJob;
-        WorkersWithMissingVaccinesReportJob::dispatch($job);
+        $queue = App::make('queue.connection');
+        $size = $queue->size();
+        $message = 'Um relatório já foi solicitado anteriormente. Aguarde alguns instantes e atualize a página';
 
-        return redirect(route('workers.non-vaccinated-report'))->with('message', 'O relatório foi solicitado e estará pronto em breve. Atualize a página para tentar novamente');
+        if ($size == 0) {
+            $job = new WorkersWithMissingVaccinesReportJob;
+            WorkersWithMissingVaccinesReportJob::dispatch($job);
+            $message = 'O relatório foi solicitado e estará pronto em breve. Atualize a página para tentar novamente';
+        }
+
+        return redirect(route('workers.non-vaccinated-report'))->with('message', $message);
     }
 
     public function getReport($type, $report)
--- resources/views/workers/report.blade.php
+++ resources/views/workers/report.blade.php
@@ -2,48 +2,58 @@
     <h1 class="text-4xl font-extrabold md:text-5xl lg:text-6xl mb-3">Relatório de Funcionários Não-Vacinados</h1>
 
 
-    @if (!empty($files))
-        Último Relatório: <i>{{$files[0]['file']}}</i>
-        <button id="getLatestReport" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 ml-3 btnDownloadFile" data-file="{{$files[0]['full_path']}}" >Baixar último relatório</button>
-    @endif
+    @if (!is_null($latestFile))
+        Último Relatório:
+        @if ($latestFile === false)
+            <i>Em processo...</i>
+        @else
+            <i>{{ $latestFile['file'] }}</i>
 
-    <br>
+            <button id="getLatestReport"
+                class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 ml-3 btnDownloadFile"
+                data-file="{{ $latestFile['full_path'] }}">Baixar último relatório</button>
+        @endif
+    @endif
 
     <form action="{{ route('workers.make-report') }}" method="POST">
         @csrf
-        <button id="makeNewReport" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 " type="submit">Gerar Novo relatório</button>
+        <button id="makeNewReport"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 "
+            type="submit">Gerar Novo relatório</button>
     </form>
     @if (Session::has('message'))
-    <div class="bg-green-100 rounded p-4 w-50">
-        <p>{{Session::get('message')}}</p>
-    </div>
+        <div class="bg-green-100 rounded p-4 w-50">
+            <p>{{ Session::get('message') }}</p>
+        </div>
     @endif
 
-    @if(!empty($files))
-    <hr class="h-px my-5 bg-gray-700 border-0 dark:bg-gray-700" />
-    <h2 class="text-2xl font-extrabold mb-3">Últimos relatórios</h2>
+    @if (!empty($files))
+        <hr class="h-px my-5 bg-gray-700 border-0 dark:bg-gray-700" />
+        <h2 class="text-2xl font-extrabold mb-3">Relatórios solicitados</h2>
 
-    <ul>
-        @foreach ($files as $file)
-            <li>
-                <i>{{ $file['file'] }}</i>
-                <button type="button" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 ml-3  dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 btnDownloadFile" data-file="{{ $file['full_path'] }}">Baixar</button>
-            </li>
-        @endforeach
-    </ul> 
+        <ul>
+            @foreach ($files as $file)
+                <li>
+                    <i>{{ $file['file'] }}</i>
+                    <button type="button"
+                        class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 ml-3  dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 btnDownloadFile"
+                        data-file="{{ $file['full_path'] }}">Baixar</button>
+                </li>
+            @endforeach
+        </ul>
     @endif
 </x-layouts.app>
 
 <script>
-document.addEventListener('DOMContentLoaded', function() {
-    let downloadButtons = document.querySelectorAll('.btnDownloadFile');
+    document.addEventListener('DOMContentLoaded', function() {
+        let downloadButtons = document.querySelectorAll('.btnDownloadFile');
         downloadButtons.forEach(element => {
             element.addEventListener('click', function() {
                 let downloadPath = element.dataset.file;
 
-                let url = '{{route("workers")}}/' + downloadPath;
+                let url = '{{ route('workers') }}/' + downloadPath;
                 window.location = url;
             })
         });
-});
+    });
 </script>
